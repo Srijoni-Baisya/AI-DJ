ac5a630e8364c792c02503a9e7c17e58f88327fe
Srijoni-Baisya <75311639+Srijoni-Baisya@users.noreply.github.com>
2021-10-01 17:55:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 //create an empty variable to store the music
 song = "";
 
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
 function preload(){
     song = loadSound("music.mp3");
 }
@@ -13,6 +19,14 @@ function setup(){
     //access the webcam
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video,modelLoaded);
+
+    poseNet.on('pose',gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet is initialized!");
 }
 
 function draw(){
@@ -20,7 +34,23 @@ function draw(){
     image(video,0,0,500,500);
 }
 
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = " + leftWristX + " , Left Wrist Y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("Right Wrist X = " + rightWristX + " , Right Wrist Y = " + rightWristY);
+    }
+}
+
 function play(){
     //play the sound
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
